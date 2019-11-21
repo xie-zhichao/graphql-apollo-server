09421ad84966ada944fa3e28e29ddce4e4f5525c
谢志超 <xiezc@sce-re.com>
2019-11-21 14:11:07 +0800

    # 修订引入方式

--- src/controllers/funHome.ts
+++ src/controllers/funHome.ts
@@ -1,6 +1,6 @@
 import { getHomePageDatas } from '../services/fun-app/homeService'
 import { ParameterizedContext } from 'koa'
-import Router = require('koa-router')
+import Router from 'koa-router'
 
 export const funHome = async (ctx: ParameterizedContext<any, Router.IRouterParamContext<any, {}>>) => {
   const { response } = ctx
