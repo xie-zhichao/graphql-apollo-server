b1f46683decdf837037cf4803e5de68b79095641
谢志超 <xiezc@sce-re.com>
2019-11-21 17:40:07 +0800

    - 添加 model - 增加 nexus 支持

--- src/loader/prisma/userLoader.ts
+++ src/loader/prisma/userLoader.ts
@@ -1,4 +1,4 @@
-import { prisma } from '../../prisma/prisma-client'
+import { prisma } from '../../_prisma_generated/prisma-client'
 
 export async function createUser() {
   const newUser = await prisma.createUser({ name: 'Alice' })
